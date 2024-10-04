c22b939aa00cb0e46e2ad05662c608d564f2dc16
unknown <jg5842486@gmail.com>
2024-10-03 19:45:08 -0500

    se agrega el proyecto

---
+++ lib_entidades/Clientes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lib_entidades
+{
+    public class Clientes
+    {
+        [Key] public int ID_Persona { get; set; }
+        public string? Cedula { get; set; }
+        public string? Nombre { get; set; }
+
+    }
+}
